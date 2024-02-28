4f830ffe49d2dfe64048727b81640618c0a0635f
Livia Belao <livia-belao@aluno.senai.br>
2024-02-28 08:46:27 -0300

    feat: Colocando texto na pag home

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -7,9 +7,32 @@ export default function Home() {
   return (
     <View>
       <Text>Liv's Coffe</Text>
+      <View>
+        <Text>
+            Bem vindo a Liv's Coffe! Espero que gostem do nosso estabelecimento.
+            Aqui no nosso site vocês encontraram tudo sobre os nossos produtos!
+        </Text>
+      </View>
+      <View>
+        <Text>
+            Nosso objetivo é trazer amor e alegria com as nossas guloseimas, espero que vocês se sintam em casa no nosso ambiente.
+        </Text>
+      </View>
       <TouchableOpacity
       onPress={()=> navigation.navigate("SobreMim")}>
         <Text>Conheça mais sobre mim!</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+      onPress={()=> navigation.navigate("Salgados")}>
+        <Text>Conheça nosso cardápio de salgados!</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+      onPress={()=> navigation.navigate("Doces")}>
+        <Text>Conheça nosso cardápio de Doces!</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+      onPress={()=> navigation.navigate("Bebidas")}>
+        <Text>Conheça nosso cardápio de Bebidas!</Text>
       </TouchableOpacity>
     </View>
   )
