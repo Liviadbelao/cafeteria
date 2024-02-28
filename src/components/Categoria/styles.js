08b68f7489373b97eb95a219f3ac2feb310dcf71
Livia Belao <livia-belao@aluno.senai.br>
2024-02-28 11:01:05 -0300

    feat: estilização componente categoria

---
+++ src/components/Categoria/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    backgroundColor: "#A52A2A",
+    width: "90%",
+    marginTop: 5,
+    marginLeft: 15,
+    borderRadius: 8,
+  },
+  texto: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 16,
+  },
+});
+export default styles;
